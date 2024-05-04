b69c05ab9e12b27b8c6d82da4bec847d8254cb5b
Guilherme V. Rocha <guilhermevieirar14@gmail.com>
2024-05-04 18:58:55 -0300

    delete file test

--- __tests__/a.spec.ts
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { describe } from "node:test";
-
-describe("A", () => {
-  render();
-  it("should render correctly", () => {});
-});
